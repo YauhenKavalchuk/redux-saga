eeaa862e5397f933e6dd14d9d58c2b61d42c6043
YauhenKavalchuk <yauhen.kavalchuk@gmail.com>
2021-10-09 12:12:38 +0300

    Add useEffect

--- src/pages/popular-news/popular-news.js
+++ src/pages/popular-news/popular-news.js
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getNews } from "../../redux/actions/actionCreator";
-import News from '../../components/news/news';
+import News from "../../components/news/news";
 
 const App = () => {
   const { popularNews } = useSelector(store => store?.news || {});
