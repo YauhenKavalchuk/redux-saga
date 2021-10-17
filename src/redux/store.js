74ec53aa9458c99e0de75dd30dfee6c25d3861e0
Yauhen Kavalchuk <yauhen.kavalchuk@gmail.com>
2021-10-17 15:55:45 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import reducer from './reducers/index';
+import reducer from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
